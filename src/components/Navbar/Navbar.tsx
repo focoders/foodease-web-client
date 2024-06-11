9cd8b6e59d27d17e26497b93de0d2d7bcc14d4f1
Aloysius Juan Farrel Lumentut <82431271+clarkkiee@users.noreply.github.com>
2024-06-11 17:36:25 +0700

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { IoPersonCircleSharp } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <nav className="flex bg-white  shadow-md">
+    <nav className="flex bg-white shadow-md">
       <div className="container mx-auto flex items-center justify-between w-[80%]">
         <Link href="/">
           <Image src='../../logos/5.svg' alt="logos" width={100} height={100}/>
@@ -29,7 +29,7 @@ export default function Navbar() {
             <input
               type="text"
               placeholder="Search for products..."
-              className="flex items-center w-[560px] px-3 py-2 bg-[#f0f0f0] rounded-full border-0 outline-none"
+              className="flex items-center w-full px-3 py-2 bg-[#f0f0f0] rounded-full border-0 outline-none"
             />
           </div>
           <div className="flex items-center">
