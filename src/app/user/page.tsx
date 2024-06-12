d9b7b098bfd17014aeea2b396545755521f8e42e
clarkkkkk <juanfarrel0404@gmail.com>
2024-06-13 00:38:32 +0700

    fix: component error resolve

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function UserHomepage() {
   return (
     <div className='flex flex-wrap justify-center p-4 lg:p-12'>
-          <div className='grid gap-2 max-w-max gap-y-6 grid-cols-2 lg:grid-cols-4 '>
+          <div className='grid gap-2 max-w-max gap-y-6 grid-cols-2 lg:grid-cols-4 lg:gap-4 '>
             <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
             <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
             <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
@@ -17,7 +17,6 @@ function UserHomepage() {
             <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
             <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
           </div>
-        
     </div>
   )
 }
--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import React from "react";
 import { useRouter } from "next/navigation";
-import Rating from "react-rating";
-import { IoMdStar, IoStarHalf, IoMdStarOutline } from "react-icons/io";
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 interface ProductCardProps {
   productId: string;
@@ -30,9 +29,17 @@ function ProductCard(props: ProductCardProps) {
     router.push(`/user/product/${productId}`);
   };
 
+  const Star = <path d="M440 60l120 336h320L618 592l94 348-272-208-272 208 94-348L0 396h320L440 60" />
+
+  const customStyles = {
+    itemShapes: Star,
+    activeFillColor: '#FFC633',
+    inactiveFillColor: '#D9D9D9',
+  };
+
   return (
     <button onClick={() => handleDetailProduct(productId)}>
-      <div className="flex flex-col p-2 border-[1px] border-[#d2d2d2] rounded-md shadow-md hover:scale-[103%] transition-all duration-400">
+      <div className="flex flex-col p-3 border-[1px] border-[#d2d2d2] rounded-md shadow-md hover:scale-[103%] transition-all duration-400">
         <div className="flex justify-center w-full">
           <Image
             src={image_url || "https://placehold.co/160x120?text=Product Image"}
@@ -42,7 +49,7 @@ function ProductCard(props: ProductCardProps) {
             className="object-cover mx-auto rounded-md lg:w-[268px] lg:h-[200px]"
           />
         </div>
-        <div className="flex flex-col gap-[6px] text-left p-2 ">
+        <div className="flex flex-col w-min gap-[6px] text-left p-2 ">
           <p className="text-m-h4 font-bold text-black truncate ...">
             {productName}
           </p>
@@ -58,15 +65,8 @@ function ProductCard(props: ProductCardProps) {
             <p className="text-black text-m-b2 line-clamp-2">{location}</p>
           </div>
           <div className="flex w-full items-center gap-2 text-m-b2">
-            <div className="flex text-[#FFC633]">
-              <Rating
-                emptySymbol={<IoMdStarOutline/>}
-                fullSymbol={<IoMdStar/>}
-                fractions={2}
-                readonly
-                initialRating={rating}
-                className="flex mt-0.5"
-              />
+            <div className="flex w-[70%]">
+              <Rating readOnly value={rating} itemStyles={customStyles} className="text-blue-100"/>
             </div>
             <div className="flex">{rating}/5</div>
           </div>
