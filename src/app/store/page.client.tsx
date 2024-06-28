a743b04a660caff46b029e3f9da8daec0ab30ae0
clarkkkkk <juanfarrel0404@gmail.com>
2024-06-28 07:10:26 +0700

    fix: type error

--- src/app/store/page.client.tsx
+++ src/app/store/page.client.tsx
@@ -6,16 +6,16 @@ function StoreHomepage() {
   return (
     <div className='flex flex-wrap justify-center p-4 lg:p-12'>
           <div className='grid gap-2 max-w-max gap-y-6 grid-cols-2 lg:grid-cols-4 lg:gap-4 '>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
-            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={undefined} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
+            <ProductCard productId='121' productName='Product Name' originPrice='13000' salePrice='8000' location='Juan Store' image_url={null} rating={4.5}/>
           </div>
     </div>
   )
